a7f29e8b37637266b2ba9db3e3e2ff1c0d27f7a2
SARA MORELL <saramorell@gmail.com>
2019-01-24 11:42:35 -0700

    edit fields populating

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 
 class Edit extends Component {
 
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.editMessage({id: this.props.messageToEdit.id, message: e.target.message.value, name:e.target.name.value})
+  }
+
   render() {
     return (
       <div>
-        <form className="row" >
+        <form className="row" onSubmit={ () => this.onSubmit }>
           <div className="col-sm">
-            <input type="text" className="form-control" name="name" id="name" />
+            <input type="text" className="form-control" name="name" id="name" defaultValue={this.props.messageToEdit.name} />
             </div>
           <div className="col-sm">
-            <input type="text" className="form-control" name="message" id="message" />
+            <input type="text" className="form-control" name="message" defaultValue={this.props.messageToEdit.message} id="message" />
           </div>
           <button type="submit" className="btn btn-secondary">submit</button>
         </form>
--- src/components/Message.js
+++ src/components/Message.js
@@ -9,12 +9,14 @@ class Message extends Component {
   }
 
   editToggle = (message) => {
-    this.setState({ editOn: !this.state.editOn })
+    let messageObj = message
+    this.setState({ editOn: !this.state.editOn, messageToEdit: messageObj })
 
   }
 
 
     render() {
+      console.log(this.state.messageToEdit)
 
         return (
           <div>
@@ -25,7 +27,7 @@ class Message extends Component {
                   <div id="name" className="col-md-2 ">{this.props.name}</div>
                   <div id="message" className="col-md-7 ">{this.props.message}</div>
                   <button id="delete" style={{marginRight: .3 +'em'}}className="col-md-1 btn btn-danger" onClick={()=>this.props.deleteMessage(this.props.id)}>delete</button>
-                  <button id="edit" className="col-md-1 btn btn-info"  onClick={()=>this.editToggle(this.props.id)}>edit</button>
+                  <button id="edit" className="col-md-1 btn btn-info"  onClick={()=>this.editToggle({id: this.props.id, name: this.props.name, message: this.props.message})}>edit</button>
                 </div>
                 <div className="col-md-6">
                   {!this.state.editOn ? <div></div> :
@@ -34,7 +36,7 @@ class Message extends Component {
                     editToggle={this.props.editToggle}
                     message={this.props.message}
                     messages={this.props.messages}
-                    messageToEdit={this.props.messageToEdit} />}
+                    messageToEdit={this.state.messageToEdit} />}
 
                 </div>
               </div>
