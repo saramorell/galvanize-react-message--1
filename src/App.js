0de5c437f321941d732f745c7e352af0078c5d37
SARA MORELL <saramorell@gmail.com>
2019-01-24 10:24:38 -0700

    add message and delete features added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Compose from './components/Compose';
 class App extends Component {
 
   state = {
-    messages:[]
+    messages:[],
+    composeOn: false
   }
 
   async componentDidMount() {
@@ -21,18 +22,64 @@ class App extends Component {
      return this.state.messages.map((m, i) => {
       return <Message {...m} key={i}
         messages={this.state.messages}
+        deleteMessage={this.deleteMessage}
        />
     })
+    this.setState({ messages:[...this.state.messages] })
   }
+
+  composeToggle = () => {
+    this.setState({ composeOn: !this.state.composeOn })
+  }
+
+  addNewMessage = async (message) => {
+    const response = await fetch('https://sm-assessment.herokuapp.com/messages', {
+        method: 'POST',
+        body: JSON.stringify(message),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      const updatedMessages = await response.json()
+      this.setState({ messages: [...this.state.messages, updatedMessages] })
+    }
+
+    deleteMessage = async (id) => {
+      await fetch(`https://sm-assessment.herokuapp.com/messages/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      this.setState({ messages: this.state.messages.filter((message) => message.id !== id) })
+    }
+
+
+
+
+
   render() {
     return (
       <div className="App">
         <div className="container">
-        <Header />
+        <Header
+          composeToggle={this.composeToggle}
+          />
         <MessageList
           renderMessages={this.renderMessages}
           messages={this.state.messages}
-         />
+          />
+          <div className="container">
+            <div className="col-md-6">
+            {this.state.composeOn &&
+              <Compose
+                addNewMessage={this.addNewMessage}
+                composeToggle={this.composeToggle}
+              />}
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
         return (
           <nav className="navbar navbar-dark bg-info">
             <div className="navbar-brand">Message Board</div>
-            <button type="button" className="btn btn-light">+</button>
+            <button type="button" className="btn btn-light" onClick={this.props.composeToggle}>+</button>
           </nav>
         )
     }
